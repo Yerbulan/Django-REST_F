5385b8697c7a6fff409ebcdb0fe9de5c856aa822
Yerbulan <bula136@gmail.com>
2023-05-04 16:30:08 +0600

    may 4

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
 
-requests.get()
+get_response = requests.get(endpoint, json = {"query": "Hello world"})
+print(get_response.json()['message'])
+print(get_response.status_code)
+
